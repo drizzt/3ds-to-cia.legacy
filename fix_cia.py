26529e768f97f8edb6b460a5e5da2977d1998306
Timothy Redaelli <timothy.redaelli@gmail.com>
2016-04-06 19:11:21 +0200

    Import from http://chunk.io/f/4c2c1f755c4e4eacaf1381183841d213.tar

---
+++ fix_cia.py
@@ -0,0 +1,60 @@
+from sys import argv
+from struct import pack, unpack
+
+f = open(argv[1], "r+b")
+xorpad = open(argv[2], "rb").read(0x400)
+
+def xor(bytes, xorpad):
+    if len(bytes) > len(xorpad):
+        raise Exception("xorpad is too small")
+
+    result = b""
+    for x in range(len(bytes)):
+        result += pack("B", bytes[x] ^ xorpad[x])
+    return result
+
+def roundup(x, y):
+    x = int(x)
+    y = int(y)
+    m = x % y
+    return x if m == 0 else (x - m + y)
+
+# first locate cxi & exheader in the cia file
+header_size = unpack('<I', f.read(4))[0]
+f.seek(0x08)
+cert_size = unpack('<I', f.read(4))[0]
+f.seek(0x0C)
+ticket_size = unpack('<I', f.read(4))[0]
+f.seek(0x10)
+tmd_size = unpack('<I', f.read(4))[0]
+cxi_ofs = roundup(header_size, 0x40) + roundup(cert_size, 0x40) + roundup(ticket_size, 0x40) + roundup(tmd_size, 0x40)
+exh_ofs = cxi_ofs + 0x200
+# extract exheader
+f.seek(exh_ofs, 0)
+exheader = f.read(0x400)
+# decrypt exheader
+f.seek(cxi_ofs + 0x118)
+titleid = f.read(8)
+exheader = xor(exheader, xorpad)
+# locate save data size in tmd
+tmd_ofs = roundup(header_size, 0x40) + roundup(cert_size, 0x40) + roundup(ticket_size, 0x40)
+f.seek(tmd_ofs)
+tmd_sig_type = unpack('>I', f.read(4))[0]
+if tmd_sig_type == 0x010000:
+  tmd_hdr_ofs = tmd_ofs + 0x240
+elif tmd_sig_type == 0x010001:
+  tmd_hdr_ofs = tmd_ofs + 0x140
+elif tmd_sig_type == 0x010002:
+  tmd_hdr_ofs = tmd_ofs + 0x80
+elif tmd_sig_type == 0x010003:
+  tmd_hdr_ofs = tmd_ofs + 0x240
+elif tmd_sig_type == 0x010004:
+  tmd_hdr_ofs = tmd_ofs + 0x140
+elif tmd_sig_type == 0x010005:
+  tmd_hdr_ofs = tmd_ofs + 0x80
+else:
+  print("HURP?")
+# fix save data size
+save_data_size = exheader[0x1C0:0x1C4]
+f.seek(tmd_hdr_ofs + 0x5A);
+f.write(save_data_size)
---
+++ fix_cxi.py
@@ -0,0 +1,39 @@
+import hashlib
+from sys import argv
+from struct import pack
+
+f = open(argv[1], "r+b")
+xorpad = open(argv[2], "rb").read(0x400)
+
+def xor(bytes, xorpad):
+    if len(bytes) > len(xorpad):
+        raise Exception("xorpad is too small")
+
+    result = b""
+    for x in range(len(bytes)):
+        result += pack("B", bytes[x] ^ xorpad[x])
+    return result
+
+def sha256(s):
+    h = hashlib.sha256()
+    h.update(s)
+    return h.digest()
+
+# get titleid
+f.seek(0x118)
+titleid = f.read(8)
+# get exheader
+f.seek(0x200)
+exheader = f.read(0x400)
+# decrypt exheader
+exheader = xor(exheader, xorpad)
+# set sd flag in exheader
+exh_flags = exheader[0xD]
+exh_flags = exh_flags | 2
+exheader = exheader[:0xD] + pack("B", exh_flags) + exheader[0xE:]
+# write back modified exheader
+f.seek(0x200)
+f.write(xor(exheader, xorpad))
+# reset the hash
+f.seek(0x160)
+f.write(sha256(exheader))
