42741a71d76a70040b52cd5d1fe85f49e6bbed76
Timothy Redaelli <timothy.redaelli@gmail.com>
2016-04-07 22:01:16 +0200

    Remove the needs of MSYS2 on Windows and add make_cia.cmd

--- README.md
+++ README.md
@@ -6,11 +6,17 @@ Just put your unpacked (`.3ds`) roms in `roms` directory and launch the script
 ```
 ./make_cia.sh
 ```
+
+or, under Windows
+```
+make_cia.cmd
+```
+
 The script will tell you what you need to do.
 The resulting CIAs will be found in `cia` directory
 
 ## Requirements
 Linux (x86_64) with python2.  
-Windows (x64) using [MSYS2](http://msys2.github.io).
+Windows (x86_64).
 
 This should works also on Linux/Windows 32bit and Mac OS X if you put the correct binaries in `tools`
---
+++ make_cia.cmd
@@ -0,0 +1,81 @@
+@ECHO OFF
+
+REM Copyright (c) 2016 Timothy Redaelli
+REM Based on contents by mid-kid
+REM Released under GPLv3+
+
+tools\win32\ncchinfo_gen.exe roms\*.3ds
+
+ECHO Copy ncchinfo.bin to your 3DS and make it generates the required xorpads
+ECHO Then copy the generated xorpads in the 'xorpads' directory
+
+PAUSE
+
+REM Verify ROMs and xorpads
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET FAIL=0
+FOR /F "tokens=*" %%r IN ('dir /s /b "roms\*.3ds"') DO (
+	@CALL :get_titleid "%%r" title_id
+
+	IF "!title_id!" == "" (
+		ECHO "%%r invalid." >&2
+		SET FAIL=1
+		EXIT /B
+	) ELSE (
+		SET xorpad=!title_id!.Main.exheader.xorpad
+		IF NOT EXIST "xorpads\!xorpad!" (
+			ECHO !xorpad! not found. Please put it into the 'xorpads' directory. >&2
+			SET FAIL=1
+		)
+	)
+)
+if NOT %FAIL% == 0 EXIT /B %FAIL%
+ENDLOCAL & SET xorpad=%xorpad%
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+FOR /F "tokens=*" %%r IN ('DIR /B "roms\*.3ds"') DO (
+	RD /Q /S _tmp 2>NUL
+	MKDIR _tmp
+        REM Extract cxi and cfa
+        tools\win32\rom_tool.exe --extract=_tmp "roms\%%r"
+        REM Remove any update data
+        DEL _tmp\*_UPDATEDATA.cfa 2>NUL
+	REM Fix cxi
+	tools\win32\fix_cxi.exe _tmp\*APPDATA.cxi "xorpads\%xorpad%"
+
+	REM Generate and fix CIA file
+	set /A i=0
+	FOR /F "tokens=*" %%c IN ('DIR /B "_tmp\*.cxi" "_tmp\*.cfa"') DO (
+		SET cmdline=!cmdline! -content ^"_tmp\%%c^":!i!:!i!
+		SET /A "i += 1"
+	)
+	tools\win32\makerom.exe -f cia -o "cia\%%~nr.cia" !cmdline!
+	tools\win32\fix_cia.exe "cia\%%~nr.cia" "xorpads\%xorpad%"
+)
+ENDLOCAL
+
+RD /Q /S _tmp 2>NUL
+
+EXIT /B %ERRORLEVEL%
+
+REM Uppercase title id (as in ncchinfo.bin)
+:get_titleid
+	SETLOCAL ENABLEDELAYEDEXPANSION
+		FOR /F "tokens=2,3,4" %%A IN ('tools\win32\rom_tool.exe -p %1') DO (
+			IF "%%A %%B"=="Title ID:" IF "!var_!" == "" SET var_=%%C
+		)
+		CALL :hex_toupper %var_% var_
+	ENDLOCAL & IF NOT "%~2"=="" SET %~2=%var_%
+	GOTO :EOF
+
+:hex_toupper
+	SETLOCAL
+		SET var_=%1
+		SET var_=%var_:a=A%
+		SET var_=%var_:b=B%
+		SET var_=%var_:c=C%
+		SET var_=%var_:d=D%
+		SET var_=%var_:e=E%
+		SET var_=%var_:f=F%
+	ENDLOCAL & IF NOT "%~2" == "" SET %2=%var_%
+	GOTO :EOF
