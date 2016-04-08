e35164c872f3cde75caebf6c495107c8fb11a34c
Timothy Redaelli <timothy.redaelli@gmail.com>
2016-04-08 21:11:25 +0200

    Remove bashisms on make_cia.sh, change license to GPLv2 and use busybox-w32 on Windows
    Busybox-w32 binary taken from http://frippery.org/busybox/ and
    compressed with upx --best --lzma

--- make_cia.sh
+++ make_cia.sh
@@ -1,23 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Copyright (c) 2016 Timothy Redaelli
 # Based on contents by mid-kid
-# Released under GPLv3+
+# Released under GPLv2
 
 set -e
-set -o pipefail
-shopt -s nullglob
 
-roms=( roms/*.3[dD][sS] )
+is_empty() {
+	! [ -f "$1" ]
+}
 
-if [ ${#roms[@]} -eq 0 ]; then
+if is_empty roms/*.3[dD][sS]; then
 	echo "No valid files in rom directory found." >&2
 	exit 1
 fi
 
-# FIXME Build 32 bit versions
 case "$OS" in
-Windows*)
+Windows_NT)
 	ncchinfo_gen=./tools/win32/ncchinfo_gen.exe
 	fix_cxi=./tools/win32/fix_cxi.exe
 	fix_cia=./tools/win32/fix_cia.exe
@@ -38,33 +37,35 @@ Windows*)
 	;;
 esac
 
-"$ncchinfo_gen" "${roms[@]}" >/dev/null
+"$ncchinfo_gen" roms/*.3[dD][sS] >/dev/null
 
 echo "Copy ncchinfo.bin to your 3DS and make it generates the required xorpads"
 echo "Then copy the generated xorpads in the 'xorpads' directory"
 
-echo "Press any key to continue..."
-read -sn 1 || true
+echo "Press enter to continue..."
+read _ || true
 
 fail=0
-for rom in "${roms[@]}"; do
+for rom in roms/*.3[dD][sS]; do
 	rom_base="${rom#roms/}"
+	rom_base="${rom_base%.3[dD][sS]}"
 
 	# Uppercase title id (as in ncchinfo.bin)
-	title_id=$("$rom_tool" -p "$rom" | awk '/^ > Title ID:/{print toupper($4); exit}') || {
+	title_id=$("$rom_tool" -p "$rom" | awk '/^ > Title ID:/{print toupper($4); exit}')
+	if [ -z "$title_id" ]; then
 		echo "$rom is corrupted" >&2
 		fail=1
 		continue
-	}
-	xorpad="$title_id.Main.exheader.xorpad"
+	fi
 
 	# Verify xorpads
-	if ! [[ -f "xorpads/$xorpad" ]]; then
+	xorpad="$title_id.Main.exheader.xorpad"
+	if ! [ -f "xorpads/$xorpad" ]; then
 		echo "$xorpad not found. Please put it into the 'xorpads' directory." >&2
 		fail=2
 		continue
 	fi
-	if (( $(stat -c "%s" "xorpads/$xorpad") != 1048576 )); then
+	if [ $(stat -c "%s" "xorpads/$xorpad") -ne 1048576 ]; then
 		echo "$xorpad must be 1MiB size." >&2
 		fail=3
 		continue
@@ -83,13 +84,14 @@ for rom in "${roms[@]}"; do
 
 	# Generate CIA file
 	i=0
+	cmdline=
 	for content in _tmp/*.cxi _tmp/*.cfa; do
-		cmdline+=(-content "$content":$i:$i)
+		cmdline="$cmdline -content "$content":$i:$i"
 		i=$((i + 1))
 	done
-	"$makerom" -v -f cia -o "cia/${rom_base%.3[dD][sS]}.cia" "${cmdline[@]}"
+	"$makerom" -v -f cia -o "cia/$rom_base.cia" $cmdline
 
-	"$fix_cia" "cia/${rom_base%.3[dD][sS]}.cia" "xorpads/$xorpad"
+	"$fix_cia" "cia/$rom_base.cia" "xorpads/$xorpad"
 done
 
 rm -rf _tmp
